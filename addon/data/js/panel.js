2be20e46a4ef43242056218801e4105795a0ff75
Shali Shaltiel <shali.shaltiel@jivesoftware.com>
2015-02-19 18:13:03 +0200

    minor refactor

--- addon/data/js/panel.js
+++ addon/data/js/panel.js
@@ -1,7 +1,4 @@
 var screen_img;
-function sendHeight() {
-    self.port.emit('heightChanged', $('.main').height());
-}
 
 
 self.port.on('init', function (data) {
@@ -15,30 +12,34 @@ self.port.on('init', function (data) {
 
 $(function () {
     // on document ready
-    $("#passcodeText").keydown(function (e) {
-        // Allow: backspace, delete, tab, escape, enter and .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-                // Allow: Ctrl+A
-            (e.keyCode == 65 && e.ctrlKey === true) ||
-                // Allow: home, end, left, right, down, up
-            (e.keyCode >= 35 && e.keyCode <= 40)) {
-            // let it happen, don't do anything
-            return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ($("#passcodeText").val().length == 5 || (
-            (e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) &&
-            (e.keyCode < 96 || e.keyCode > 105))) {
-            e.preventDefault();
-        }
-    });
+    $("#passcodeText").keydown(onCodeKeyDown);
 
-    $(".close").click(function (e) {
+    $(".close").click(function () {
         self.port.emit('close', null);
     });
 
     $('#screenshot').load(sendHeight);
 
     sendHeight();
-
 });
+
+function sendHeight() {
+    self.port.emit('heightChanged', $('.main').height());
+}
+function onCodeKeyDown(e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl+A
+        (e.keyCode == 65 && e.ctrlKey === true) ||
+            // Allow: home, end, left, right, down, up
+        (e.keyCode >= 35 && e.keyCode <= 40)) {
+        // let it happen, don't do anything
+        return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ($("#passcodeText").val().length == 5 || (
+        (e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) &&
+        (e.keyCode < 96 || e.keyCode > 105))) {
+        e.preventDefault();
+    }
+}
