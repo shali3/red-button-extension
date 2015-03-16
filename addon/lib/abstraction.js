0120686ead156c46928b925195ced27b119dcbb2
Shali Shaltiel <shali.shaltiel@jivesoftware.com>
2015-03-16 13:32:06 +0200

    Added 2 minutes limit.

--- addon/lib/abstraction.js
+++ addon/lib/abstraction.js
@@ -6,11 +6,23 @@ exports.saveReport = function (id, code) {
 
     var myReport = {
         reportID: id,
-        reportCode: code
+        reportCode: code,
+        time: new Date().getTime()
     };
     reports.push(myReport);
     if (reports.length > 10) {
         reports = reports.slice(1);
     }
     ss.storage.reports = reports;
+};
+
+exports.canReport = function () {
+    var reports = ss.storage.reports;
+    if (!reports)
+        return true;
+    var lastTime = reports[reports.length - 1].time;
+    if (lastTime % 1 === 0)
+        return (new Date().getTime()) - lastTime > 120000;
+    else
+        return true;
 };
--- addon/lib/service.js
+++ addon/lib/service.js
@@ -1,20 +1,25 @@
 var abstraction = require("./abstraction");
 var Request = require("sdk/request").Request;
+var _ = require("sdk/l10n").get;
 
 exports.postReport = function (data, success, failure) {
-    Request({
-        url: "http://redbuttonproject.org/ReportHandler.ashx",
-        //url: data.comment.length > 0 ? "http://localhost:8000/test1" : "http://localhost:8000/test2",
-        content: data,
-        onComplete: function (response) {
-            console.log('Got Response ' + response.status + ' text: ' + response.text);
-            if (response.status == 200) {
-                abstraction.saveReport(response.text, data.code);
-                success(response.text, data.code);
+    if (abstraction.canReport()) {
+        Request({
+            url: "http://redbuttonproject.org/ReportHandler.ashx",
+            //url: data.comment.length > 0 ? "http://localhost:8000/test1" : "http://localhost:8000/test2",
+            content: data,
+            onComplete: function (response) {
+                console.log('Got Response ' + response.status + ' text: ' + response.text);
+                if (response.status == 200) {
+                    abstraction.saveReport(response.text, data.code);
+                    success(response.text, data.code);
+                }
+                else {
+                    failure(response.status);
+                }
             }
-            else {
-                failure(response.status);
-            }
-        }
-    }).post();
+        }).post();
+    } else {
+        failure(_("report_limit_error"));
+    }
 };
