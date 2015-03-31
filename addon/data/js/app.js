55e64c6977a484cc25e24022a50bade63077170c
Shali Shaltiel <shali.shaltiel@jivesoftware.com>
2015-03-31 17:59:41 +0300

    Some more WIP

---
+++ addon/data/js/app.js
@@ -0,0 +1,16 @@
+(function () {
+    var app = angular.module('app', ['ui.bootstrap']);
+    app.controller('TabsCtrl', function ($scope, $window) {
+        $scope.tabs = [
+            {title: 'Dynamic Title 1', content: 'Dynamic content 1'},
+            {title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true}
+        ];
+
+        $scope.alertMe = function () {
+            setTimeout(function () {
+                $window.alert('You\'ve selected the alert tab!');
+            });
+        };
+
+    });
+})();
