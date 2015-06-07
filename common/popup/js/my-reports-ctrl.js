54aed2bf8cab38ebd56dc7f77e278d9ce973f746
shali3 <shali3@gmail.com>
2015-06-07 22:30:58 +0300

    getting reports to the app

--- common/popup/js/my-reports-ctrl.js
+++ common/popup/js/my-reports-ctrl.js
@@ -4,8 +4,15 @@
 'use strict';
 function MyReportsCtrl($scope, backgroundPage) {
     $scope.openedItem = 0;
+    $scope.loading = true;
     backgroundPage.getReports().then(function (reports) {
         $scope.reports = reports;
+
+    }, function (error) {
+        $scope.error = error;
+    }).finally(function () {
+        $scope.loading = false;
+
     });
 }
 app.controller('MyReportsCtrl', MyReportsCtrl);
